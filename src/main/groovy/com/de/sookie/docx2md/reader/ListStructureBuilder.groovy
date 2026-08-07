3a2b324413aff8727a3cc71c86168455a0896e16
Markus Meixner <markus.meixner@nterra.com>
2026-08-07 10:54:30 +0200

    Anpassung

--- src/main/groovy/com/de/sookie/docx2md/reader/ListStructureBuilder.groovy
+++ src/main/groovy/com/de/sookie/docx2md/reader/ListStructureBuilder.groovy
@@ -14,7 +14,20 @@ class ListStructureBuilder {
         blocks.each { block ->
 
             if (block instanceof Paragraph && block.listId) {
-                handleParagraph(block, result, stack)
+                ListItem item = createItem(block)
+
+                while (!stack.isEmpty() &&
+                        stack.last().level >= item.level) {
+                    stack.remove(stack.size() - 1)
+                }
+
+                if (stack.isEmpty()) {
+                    result << item
+                } else {
+                    stack.last().add(item)
+                }
+
+                stack << item
                 return
             }
 
@@ -34,63 +47,13 @@ class ListStructureBuilder {
         return result
     }
 
-    private void handleParagraph(
-            Paragraph paragraph,
-            List<Block> result,
-            List<ListItem> stack
-    ) {
-        if (stack.isEmpty()) {
-            createListItem(paragraph, result, stack)
-            return
-        }
+    private ListItem createItem(Paragraph paragraph) {
+        ListItem item = new ListItem(
+                listId: paragraph.listId,
+                level: paragraph.listLevel
+        )
 
-        ListItem current = stack.last()
-
-        if (current.listId != paragraph.listId) {
-            stack.clear()
-            createListItem(paragraph, result, stack)
-            return
-        }
-
-        if (current.level < paragraph.listLevel) {
-            ListItem child = new ListItem()
-            child.listId = paragraph.listId
-            child.level = paragraph.listLevel
-            child.add(paragraph)
-            current.add(child)
-            stack.add(child)
-            return
-        }
-
-        if (current.level == paragraph.listLevel) {
-            createListItem(paragraph, result, stack)
-            return
-        }
-
-        while (!stack.isEmpty() &&
-                stack.last().level >= paragraph.listLevel) {
-            stack.remove(stack.size() - 1)
-        }
-
-        createListItem(paragraph, result, stack)
-    }
-
-    private void createListItem(
-            Paragraph paragraph,
-            List<Block> result,
-            List<ListItem> stack
-    ) {
-        ListItem item = new ListItem()
-        item.listId = paragraph.listId
-        item.level = paragraph.listLevel
         item.add(paragraph)
-
-        if (stack.isEmpty()) {
-            result << item
-        } else {
-            stack.last().add(item)
-        }
-
-        stack.add(item)
+        return item
     }
 }
